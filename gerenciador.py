6fe53f0f402db738e28aeb3bdf4f1bbc024ff1fd
vhborges <viniciusborges22@gmail.com>
2019-06-25 19:11:35 -0300

    Organize methods

--- gerenciador.py
+++ gerenciador.py
@@ -13,16 +13,16 @@ class Gerenciador(Componente):
     
     def iniciaThreads(self, portas, gerenciadorPronto):
         servidorTempPronto = Event()
-        sensorTemp = Thread(target=self.processaSensor, args=(portas[0], servidorTempPronto,))
+        sensorTemp = Thread(target=self.processaSocket, args=(portas[0], servidorTempPronto,))
 
         servidorUmidPronto = Event()
-        sensorUmid = Thread(target=self.processaSensor, args=(portas[1], servidorUmidPronto,))
+        sensorUmid = Thread(target=self.processaSocket, args=(portas[1], servidorUmidPronto,))
 
         servidorCO2Pronto = Event()
-        sensorCO2 = Thread(target=self.processaSensor, args=(portas[2], servidorCO2Pronto,))
+        sensorCO2 = Thread(target=self.processaSocket, args=(portas[2], servidorCO2Pronto,))
 
         servidorClientePronto = Event()
-        cliente = Thread(target=self.processaCliente, args=(portas[3], servidorClientePronto,))
+        cliente = Thread(target=self.processaCliente, args=(servidorClientePronto,))
 
         sensorTemp.start()
         sensorUmid.start()
@@ -38,8 +38,8 @@ class Gerenciador(Componente):
         sensorUmid.join()
         sensorCO2.join()
         cliente.join()
-
-    def processaSensor(self, porta, servidorPronto):
+    
+    def processaSocket(self, porta, servidorPronto):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serv:
             serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             serv.bind((self.host, porta))
@@ -55,19 +55,30 @@ class Gerenciador(Componente):
                 while not conectado.is_set():
                     mensagem = self.recebeMensagem(conexao)
                     print('mensagem recebida', mensagem['tipo'], mensagem['id_mensagem'], mensagem['id_componente'])
-                    self.processaMensagem(mensagem, conexao, conectado, recebendo)
+                    self.processaMensagem(mensagem, conexao, conectado)
+
+                solicitaLeitura = self.geraMensagem(tipo='EVG', id_mensagem='0', id_componente=mensagem['id_componente'])
+                conexao.sendall(solicitaLeitura)
+                recebendo.set()
                 
                 while conectado.is_set():
-                    while not recebendo.is_set():
-                        mensagem = self.recebeMensagem(conexao)
-                        print('mensagem recebida', mensagem['tipo'], mensagem['id_mensagem'], mensagem['id_componente'])
-                        self.processaMensagem(mensagem, conexao, conectado, recebendo)
-                    
-                    while recebendo.is_set():
-                        mensagem = self.recebeMensagem(conexao)
-                        self.processaMensagem(mensagem, conexao, conectado, recebendo)
+                    if int(mensagem['id_componente']) in range(1, 4):
+                        self.processaSensor(conexao, conectado, recebendo)
+                    elif int(mensagem['id_componente']) in range(4, 8):
+                        self.processaAtuador(conexao, conectado)
+                    elif int(mensagem['id_componente']) == 8:
+                        self.processaCliente(servidorPronto)
+                
+
+    def processaSensor(self, conexao, conectado, recebendo):
+        while recebendo.is_set():
+            mensagem = self.recebeMensagem(conexao)
+            self.processaMensagem(mensagem, conexao, conectado)
     
-    def processaCliente(self, porta, servidorPronto):
+    def processaAtuador(self, conexao, conectado):
+        pass
+
+    def processaCliente(self, servidorPronto):
         servidorPronto.set()
         while True:
             sleep(2)
@@ -75,7 +86,7 @@ class Gerenciador(Componente):
             print('Umidade:', self.umidade)
             print('CO2:', self.co2)
     
-    def processaMensagem(self, mensagem, conexao, conectado, recebendo):
+    def processaMensagem(self, mensagem, conexao, conectado):
         if (mensagem['tipo'] == 'EVG' and mensagem['id_mensagem'] == '1'):
             if mensagem['id_componente'] == '1':
                 self.temperatura = mensagem['valor']
@@ -88,6 +99,3 @@ class Gerenciador(Componente):
             confirmaConexao = self.geraMensagem(tipo='COS', id_mensagem='1', id_componente=mensagem['id_componente'])
             conexao.sendall(confirmaConexao)
             conectado.set()
-            solicitaLeitura = self.geraMensagem(tipo='EVG', id_mensagem='0', id_componente=mensagem['id_componente'])
-            conexao.sendall(solicitaLeitura)
-            recebendo.set()
